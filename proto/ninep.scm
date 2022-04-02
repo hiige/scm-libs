d3a94312a300e8d50b2d0dae01e5418d84c031b7
hiige <hh.ge@tutanota.com>
2022-04-02 12:44:54 +0000

    add acme shortcuts

--- proto/ninep.scm
+++ proto/ninep.scm
@@ -1,13 +1,33 @@
-(define (ninep-read . a)
+(define (ninep-read n)
   (call-with-process-io
-   (cons "9p" (cons "read" a))
+   (list "9p"
+         "read"
+         n)
    (lambda (pid in out err)
      (port->string out))))
 
-(define (ninep-write . a)
+(define (ninep-acme-read n)
+  (ninep-read (string-append "acme/"
+                             n)))
+
+(define (ninep-awin-read n)
+  (ninep-acme-read (string-append "$WINID/"
+                                  n)))
+
+(define (ninep-write n str)
   (call-with-process-io
-   (cons "9p"
-    (cons "write"
-     (drop-right a 1)))
-   (lambda (pid in out err)
-     (write-string (last a)))))
+    (list "9p"
+          "write"
+          n)
+    (lambda (pid in out err)
+      (write-string str))))
+
+(define (ninep-acme-write n str)
+  (ninep-write (string-append "acme/"
+                               n)
+                str))
+
+(define (ninep-awin-write n str)
+  (ninep-acme-write (string-append "$WINID/"
+                                   n)
+                    str))
--- proto/ninep.sld
+++ proto/ninep.sld
@@ -1,5 +1,7 @@
 (define-library (scm proto ninep)
- (export ninep-write ninep-read)
+ (export ninep-write ninep-read
+   ninep-acme-write ninep-acme-read
+   ninep-awin-write ninep-awin-read)
   (import
    (chibi) (chibi io)
    (chibi process) (srfi 1))
